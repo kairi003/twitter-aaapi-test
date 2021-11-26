c5f543b846b1c03694a11450d8540a6b8be448fe
Glitch (brawny-erratic-pluto) <none>
2021-11-26 14:27:11 +0000

    👡🙏 Checkpoint ./main.py:20293692/322

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import hashlib
 import hmac
 import json
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 CONSUMER_KEY = os.environ['CONSUMER_KEY']
@@ -20,12 +20,14 @@ def top():
 def webhook_challenge():
     crc_token = request.args.get('crc_token', '')
     digest = hmac.new(CONSUMER_SECRET.encode(), crc_token.encode(), hashlib.sha256).digest()
-    response = {'response_content': 'sha256=' + base64.b64encode(digest).decode()}
-    return json.dumps(response)
+    content = {'response_token': 'sha256=' + base64.b64encode(digest).decode()}
+    return jsonify(content), 200
 
 
 @app.route('/webhooks/twitter', methods=['POST'])
 def webhook():
-    return ''
+    response = make_response('', 200)
+    response.mimetype = "text/plain"
+    return response
 
 app.run(port=8000)
