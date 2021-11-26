38696a5c57e1bc2bc2ef78873e5aa6e80d3e3adf
kairi003 <kairi.satellite@gmail.com>
2021-11-26 22:32:17 +0900

    Update webhook_challenge

--- main.py
+++ main.py
@@ -10,12 +10,22 @@ app = Flask(__name__)
 CONSUMER_KEY = os.environ['CONSUMER_KEY']
 CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
 
+
 @app.route('/')
 def top():
-  return 'hello'
+    return 'hello'
 
-@app.route('/webhooks/twitter', methods=['GET', 'POST'])
+
+@app.route('/webhooks/twitter', methods=['GET'])
+def webhook_challenge():
+    crc_token = request.args.get('crc_token', '')
+    digest = hmac.new(CONSUMER_SECRET.encode(), crc_token.encode(), hashlib.sha256).digest()
+    response = {'response_content': 'sha256=' + base64.b64encode(digest)}
+    return json.dumps(response)
+
+
+@app.route('/webhooks/twitter', methods=['POST'])
 def webhook():
-    return 'Hello'
+    return ''
 
 app.run(port=8000)
